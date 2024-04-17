7bad496daa9adb8b17dfacaf0170689c57f1816a
Li Mengyu <aumengyu@gmail.com>
2024-04-17 23:41:20 +0930

    update

--- src/components/SelectImagePage.js
+++ src/components/SelectImagePage.js
@@ -4,15 +4,15 @@ function SelectImagePage({ images, onSelect, selectedItems, step }) {
   return (
     <div className="container">
       <h1>Choose an image - Step {step}</h1>
-      <div className="row">
+      <div className="row" style={{ marginRight: '-5px', marginLeft: '-5px' }}>
         {images.map((image, index) => (
-          <div key={index} className="col-md-2 col-6" style={{ marginBottom: '20px' }}> 
+          <div key={index} className="col-md-2 col-6" style={{ padding: '8px' }}>
             <img
               src={image.src}
               alt={image.alt}
               className={`img-fluid ${selectedItems.includes(image.alt) ? 'selected' : ''}`}
               onClick={() => onSelect(image)}
-              style={{ cursor: 'pointer', border: selectedItems.includes(image.alt) ? '3px solid blue' : 'none' }}
+              style={{ cursor: 'pointer', border: selectedItems.includes(image.alt) ? '3px solid blue' : 'none', width: '100%', height: 'auto' }}
             />
           </div>
         ))}
