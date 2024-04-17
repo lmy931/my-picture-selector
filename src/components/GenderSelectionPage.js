72467ceef44b68973da53e27716e4329ee7b27fc
Li Mengyu <aumengyu@gmail.com>
2024-04-18 08:49:20 +0930

    update

--- src/components/GenderSelectionPage.js
+++ src/components/GenderSelectionPage.js
@@ -3,12 +3,12 @@ import React from 'react';
 function GenderSelectionPage({ onSelectGender }) {
   return (
     <div className="text-center">
-      <h1>找到真正属于您</h1><h1>的香氛</h1>
+      <h1>找到真正属于您的香氛</h1>
       <h4 style={{ marginTop: '40px' }}>WHO ARE YOU?</h4>
-      <div className="btn-group" role="group" style={{ marginTop: '40px' }}>
-        <button className="btn btn-primary" onClick={() => onSelectGender('Man')}>Man</button>
-        <button className="btn btn-primary" onClick={() => onSelectGender('Woman')}>Woman</button>
-        <button className="btn btn-primary" onClick={() => onSelectGender("It doesn't matter")}>It doesn't matter</button>
+      <div className="d-flex justify-content-center" style={{ marginTop: '40px' }}>
+        <button className="btn btn-custom flex-fill mx-1" onClick={() => onSelectGender('Man')}>Man</button>
+        <button className="btn btn-custom flex-fill mx-1" onClick={() => onSelectGender('Woman')}>Woman</button>
+        <button className="btn btn-custom flex-fill mx-1" onClick={() => onSelectGender("It doesn't matter")}>It doesn't matter</button>
       </div>
     </div>
   );
