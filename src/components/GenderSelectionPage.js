5deb9fd5eeb605c4881c5e58a445300a33cac389
Li Mengyu <aumengyu@gmail.com>
2024-04-18 09:00:22 +0930

    update

--- src/components/GenderSelectionPage.js
+++ src/components/GenderSelectionPage.js
@@ -5,9 +5,9 @@ function GenderSelectionPage({ onSelectGender }) {
     <div className="text-center">
       <h1>找到真正属于您的香氛</h1>
       <h4 style={{ marginTop: '40px' }}>WHO ARE YOU?</h4>
-      <div className="d-flex justify-content-center" style={{ marginTop: '40px' }}>
-        <button className="btn btn-custom flex-fill mx-1" onClick={() => onSelectGender('Man')}>Man</button>
-        <button className="btn btn-custom flex-fill mx-1" onClick={() => onSelectGender('Woman')}>Woman</button>
+      <div className="d-flex flex-column flex-md-row justify-content-center" style={{ marginTop: '40px' }}>
+        <button className="btn btn-custom flex-fill mx-1 mb-2 mb-md-0" onClick={() => onSelectGender('Man')}>Man</button>
+        <button className="btn btn-custom flex-fill mx-1 mb-2 mb-md-0" onClick={() => onSelectGender('Woman')}>Woman</button>
         <button className="btn btn-custom flex-fill mx-1" onClick={() => onSelectGender("It doesn't matter")}>It doesn't matter</button>
       </div>
     </div>
