fb61cf59f49da44615b5fc2a578153468e49a7c2
Li Mengyu <aumengyu@gmail.com>
2024-04-18 21:08:07 +0930

    update

--- src/components/CompletionPage.js
+++ src/components/CompletionPage.js
@@ -1,28 +1,27 @@
 import React from 'react';
 
-function CompletionPage({ selectedImages }) {
-  // 选择随机图片数量（比如随机选择 3 张图片）
-  const randomImages = selectedImages.sort(() => 0.5 - Math.random()).slice(0, 3);
-  console.log(selectedImages);
+function CompletionPage({ selectedImages, gender }) {
+  const displayedImages = selectedImages.sort(() => 0.5 - Math.random()).slice(0, 3);
+
   return (
-    <div className="completion-page" style={{ position: 'relative', height: '100vh', width: '100vw', backgroundColor: 'white' }}>
-      {randomImages.map((img, index) => (
-        <img
-          key={index}
-          src={img.src}
-          alt={img.alt}
-          style={{
-            position: 'absolute',
-            left: `${Math.random() * 80}%`, // Random horizontal placement
-            top: `${Math.random() * 80}%`, // Random vertical placement
-            transform: `rotate(${Math.random() * 360 - 180}deg)`, // Random rotation
-            width: '200px',
-            height: '200px',
-            objectFit: 'cover'
-          }}
-        />
-      ))}
-      <h2 style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', color: 'red' }}>You Are...</h2>
+    <div className="completion-page" style={{ position: 'relative', height: '100vh', backgroundColor: 'black', padding: '20px' }}>
+      <h2 style={{ color: 'red', position: 'absolute', top: '5%', left: '5%', fontSize: '4em' }}>You Are...</h2>
+      <div className="row no-gutters" style={{ position: 'absolute', top: '25%', width: '100%' }}>
+        {displayedImages.map((img, index) => (
+          <div key={index} className={`col-4`} style={{ padding: '5px', display: 'flex', justifyContent: 'center' }}>
+            <img
+              src={img}
+              alt={`Selected image ${index + 1}`}
+              className="img-responsive"
+              style={{
+                transform: `rotate(${Math.random() * 40 - 20}deg)`,
+              }}
+            />
+          </div>
+        ))}
+      </div>
+      {/* 调整 top 属性以减少标题与图片之间的距离 */}
+      <h1 style={{ position: 'absolute', top: '50%', width: '100%', textAlign: 'center', color: 'red' }}>Our Suggestions</h1> {/* 确保标题位于图片下方 */}
     </div>
   );
 }
